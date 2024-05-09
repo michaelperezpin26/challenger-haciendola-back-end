5bc9f7cfe9a4750de725f06a518c3b3caebddd3e
michaelperezpin26 <perezpinmichael@gmail.com>
2024-05-09 11:31:14 -0500

    [HCDL-001] Feature: config basic in micro service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import helmet from 'helmet'; //protección a ciertos ataques de inyección 
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { json, urlencoded } from 'express';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{
+    bufferLogs: true,
+    cors: {
+      origin: true,
+      preflightContinue: false,
+    }
+  });
+  app.enable('trust proxy')
+  app.use(helmet());
+  app.use(json({ limit: '50mb'}))
+  app.use(urlencoded({ limit: '50mb', extended: true}))
+  app.useGlobalPipes(new ValidationPipe({ transform: true}))
   await app.listen(3000);
 }
 bootstrap();
